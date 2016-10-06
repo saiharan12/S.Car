8b6cd7e6924052173a632976c7ee0ce4e5d79889
saiharan12 <saiharan12@gmail.com>
2016-10-06 20:34:09 +0530

    Sai update 1.py
    Nothing but a comment.

--- S.car.py
+++ S.car.py
@@ -3,3 +3,4 @@ top = tkinter.Tk()
 #this imports the tkinter module saiharan
 # top.mainloop() makes a window
 # http://www.tkdocs.com/tutorial/
+#I got it and it is pretty easy to program in github!!!
