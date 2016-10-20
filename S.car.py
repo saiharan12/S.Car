eb94c431c291e3a4ec6f27efad411e3ed9b9f4e7
Eshanzap <eshanmind@gmail.com>
2016-10-20 20:01:33 +0530

    speedreg() and init()

--- S.car.py
+++ S.car.py
@@ -3,51 +3,65 @@ import random
 import time
 import tkinter
 #top = tkinter.Tk()
-#this imports the tkinter module saiharan
-# top.mainloop() makes a window
-# http://www.tkdocs.com/tutorial/
 #i'm starting now-eshan
 
 caroff = False
 stblt = False
-speedcalc = input("Enter the speed of the vehicle in numerical format")
-speed = int(speedcalc)
+
+speed = "a"
 brake = False
-sx = 1
+sx = 0
+seatbeltbegintime = 0
+var = 1
+def init():
+    print("Have a nice drive!")
+    print("the time and date is "+ str(time.ctime()))
+    inittime = str(time.ctime())
+def speedreg(speed):
+    print("enter the speed of the vehicle in numerical format")
+    speed = int(speed)
+    if speed >= 120:
+            print("SLOW DOWN: braking in...")
+            for x in range(1,7):
+                if x < 6:
+                    timercount = 6-x
+                    print(str(timercount)+ " seconds...")
+                    time.sleep(1)
+                    x = x+1
+                else:
+                    brake = True
+                    print("Brake toggled")
+                    speed = 80
+                    print("Speed -> 80")
+                    break
+        
+def stopcar():
+    print("Stopping Car")
+    caroff = True
+    
 def seatbelt():
     stbltinput = input("enter the seatbelt state(on/off)")
     if stbltinput == "on":
         stblt = True
-        now = time.time()
-        if sx == 1:
-           print("Have a nice drive!")
-           print("the time and date is "+ str(time.ctime()))
-        sx = sx+1
+        seatbeltbegintime = time.time()
+        print("drive safely!")
         
     elif stbltinput == "off":
         stblt = False
         if speed < 6:
-            print("SLOW DOWN: braking in...")
-            for x in range(1,7):
-                if x > 5:
-                    print("Car stopping...")
-                    brake = True
-                    break
-                print(str(x)+ " seconds...")
-                time.sleep(1)
-                x = x+1
-                
-        else:
-            caroff = True
-            print("Stopping car...")
-
-
-            
+            print("Drive slowly!")
+            return 0
+           
 def main():
-    stbltontime = time.time() - now
+    init()
+    speedreg()
+    seatbelt()
+    stbltontime = time.time() - seatbeltbegintime
     
-            
-seatbelt()
+    
+         
+main()
+ 
 
     
 
