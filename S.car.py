de589a6400395c9d285be820ca2b4c764736a3a9
saiharan12 <saiharan12@gmail.com>
2016-10-21 21:28:51 +0530

    Looped single variable version.
    Pls see this
    I will make a update for the tkinter one too...

--- S.car.py
+++ S.car.py
@@ -7,62 +7,58 @@ import tkinter
 
 caroff = False
 stblt = False
-
-speed = "a"
+speed = 0
 brake = False
 sx = 0
 seatbeltbegintime = 0
-var = 1
-def init():
-    print("Have a nice drive!")
-    print("the time and date is "+ str(time.ctime()))
-    inittime = str(time.ctime())
-def speedreg(speed):
-    print("enter the speed of the vehicle in numerical format")
-    speed = int(speed)
-    if speed >= 120:
-            print("SLOW DOWN: braking in...")
-            for x in range(1,7):
-                if x < 6:
-                    timercount = 6-x
-                    print(str(timercount)+ " seconds...")
-                    time.sleep(1)
-                    x = x+1
-                else:
-                    brake = True
-                    print("Brake toggled")
-                    speed = 80
-                    print("Speed -> 80")
-                    break
-        
+
+print("Have a nice drive!")
+print("The time and date is "+ str(time.ctime()))
+inittime = str(time.ctime())
+
+def speedreg(speedn):
+    if speedn >= 120:
+        print("SLOW DOWN: braking in...\n")
+        for x in range(1,7):
+            if x < 6:
+                timercount = 6-x
+                print(str(timercount)+ " seconds...")
+                time.sleep(1)
+                x = x+1
+            else:
+                brake = True
+                print("Brake toggled\n")
+                speed = 80
+                print("Speed -> 80\n")
+                break
+
 def stopcar():
-    print("Stopping Car")
+    print("Stopping Car\n")
     caroff = True
-    
-def seatbelt():
-    stbltinput = input("enter the seatbelt state(on/off)")
+
+def seatbelt(speedn):
+    stbltinput = input("enter the seatbelt state(on/off)\n")
     if stbltinput == "on":
         stblt = True
         seatbeltbegintime = time.time()
-        print("drive safely!")
-        
+        print("drive safely!\n")
     elif stbltinput == "off":
         stblt = False
-        if speed < 6:
-            print("Drive slowly!")
-            return 0
-           
-def main():
-    init()
-    while var == 1:
-        speedreg()
-    seatbelt()
-    stbltontime = time.time() - seatbeltbegintime
-    
-    
-         
-main()
- 
+        if speedn >= 6:
+            print("Please wear seatbelt. Braking in...\n")
+            for y in range(1,7):
+                if y < 6:
+                    timercount = 6-y
+                    print(str(timercount)+ " seconds...")
+                    time.sleep(1)
+                    y = y + 1
 
-    
+def main():
+    speed = input("Enter the speed of the vehicle in numerical format\n")
+    speedn = int(speed)
+    speedreg(speedn)
+    seatbelt(speedn)
+loop=1
+while loop==1:
+    main()
 
