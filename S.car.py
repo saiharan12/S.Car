560b43d1ae55f99d99838e81ee8a9d03cbb2871e
Eshanzap <eshanmind@gmail.com>
2016-10-23 15:16:08 +0530

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 3932a6abd7b5f2fe2b7073fa519cb2fc230c7a38, reversing
    changes made to eb94c431c291e3a4ec6f27efad411e3ed9b9f4e7.

--- S.car.py
+++ S.car.py
@@ -1,14 +1,8 @@
 import math
 import random
 import time
-<<<<<<< HEAD
 import tkinter
 #top = tkinter.Tk()
-=======
-#this imports the tkinter module saiharan
-# top.mainloop() makes a window
-# http://www.tkdocs.com/tutorial/
->>>>>>> origin/master
 #i'm starting now-eshan
 
 caroff = False
@@ -46,10 +40,9 @@ def stopcar():
     caroff = True
     
 def seatbelt():
-    stbltinput = input("Enter the seatbelt state(on/off)")
+    stbltinput = input("enter the seatbelt state(on/off)")
     if stbltinput == "on":
         stblt = True
-<<<<<<< HEAD
         seatbeltbegintime = time.time()
         print("drive safely!")
         
@@ -69,24 +62,6 @@ def main():
          
 main()
  
-=======
-    elif stbltinput == "off":
-        stblt = False
-        now=time.time()
-        if speed > 6:
-            print("SLOW DOWN: braking in...")
-            for x in range(1,7):
-                if x > 5:
-                    print("Car stopping...")
-                    brake = True
-                    print(str(x)+ " seconds...")
-                    time.sleep(1)
-                    x = x + 1
-                else:
-                    caroff = True
-                    print("Stopping car...")
-
->>>>>>> origin/master
 
     
 
